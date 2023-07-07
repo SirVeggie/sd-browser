0c2553df0bc14f993b04142c4d6da86910188c1e
Veikka <32365239+SirVeggie@users.noreply.github.com>
2023-07-07 14:55:27 +0300

    file caching

--- src/lib/server/filemanager.ts
+++ src/lib/server/filemanager.ts
@@ -8,11 +8,23 @@ import exifr from 'exifr';
 import { getPositivePrompt } from '$lib/tools/metadataInterpreter';
 
 let imageList: ImageList = new Map();
+const datapath = './localData';
 
 export async function IndexFiles() {
     console.log('Indexing files...');
+    fs.mkdir(datapath).catch(() => console.log('Failed to create data folder, might already exist'));
+    
+    // Read cached data
+    let images: ImageList;
+    const cachefile = path.join(datapath, 'metadata.json');
+    try {
+        const cache = await fs.readFile(cachefile);
+        images = new Map(JSON.parse(cache.toString()));
+    } catch {
+        images = new Map();
+    }
+    
     const dirs: string[] = [IMG_FOLDER];
-    const images: ImageList = new Map();
 
     while (dirs.length > 0) {
         const dir = dirs.pop();
@@ -23,6 +35,8 @@ export async function IndexFiles() {
         for (const file of files.filter(x => x.endsWith('.png'))) {
             const fullpath = path.join(dir, file);
             const hash = hashString(fullpath);
+            if (images.has(hash)) continue;
+            
             const metadata = await readMetadata(fullpath);
             images.set(hash, {
                 id: hash,
@@ -39,6 +53,9 @@ export async function IndexFiles() {
             if (stats.isDirectory()) dirs.push(fullpath);
         }
     }
+    
+    console.log('Writing cache file...');
+    fs.writeFile(cachefile, JSON.stringify([...images], null, 2)).catch(e => console.log(e));
 
     imageList = images;
     console.log(`Indexed ${imageList.size} images`);
@@ -110,21 +127,22 @@ function hashString(filepath: string) {
 
 async function readMetadata(imagepath: string): Promise<Partial<ServerImage>> {
     try {
-        const file = await fs.readFile(imagepath);
         const stats = await stat(imagepath);
-        
         const res: Partial<ServerImage> = {
             modifiedDate: stats.mtimeMs,
             createdDate: stats.birthtimeMs,
         };
         
-        const metadata = await exifr.parse(file);
+        const file = await fs.readFile(imagepath);
+        let metadata = await exifr.parse(file);
         if (!metadata)
             return {};
-        return {
+        metadata = {
             ...res,
             prompt: metadata.parameters ?? undefined
         } satisfies Partial<ServerImage>;
+        
+        return metadata;
     } catch {
         console.log(`Failed to read metadata for ${imagepath}`);
         return {};
