d36f1a6ba0b07414b5969b4083018b1fa8d4c4af
SirVeggie <32365239+SirVeggie@users.noreply.github.com>
2023-07-28 23:33:11 +0300

    taller nav buttons

--- src/lib/components/NavArrows.svelte
+++ src/lib/components/NavArrows.svelte
@@ -8,46 +8,81 @@
 </script>
 
 {#if left && enabled}
-    <button class="left" class:hidden on:click={onLeft} on:touchstart={onLeft}>{"<"}</button>
+    <button class="left" class:hidden on:click={onLeft} on:touchstart={onLeft}>
+        <div>{"<"}</div>
+    </button>
 {/if}
+
 {#if right && enabled}
-    <button class="right" class:hidden on:click={onRight} on:touchstart={onRight}>{">"}</button>
+    <button
+        class="right"
+        class:hidden
+        on:click={onRight}
+        on:touchstart={onRight}
+    >
+        <div>{">"}</div>
+    </button>
 {/if}
 
 <style lang="scss">
     button {
         text-decoration: none;
         appearance: none;
+        background-color: transparent;
+        border: none;
+        padding: 0;
+        margin: 0;
+        cursor: pointer;
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
         height: max(10vw, 10vh);
         width: max(5vw, 5vh);
-        background-color: #000a;
         border: none;
         color: #fffa;
+        background-color: #000a;
     }
 
     .left {
         position: fixed;
-        top: 50%;
+        top: 0;
+        bottom: 0;
         left: 0;
-        transform: translateY(-50%);
-        z-index: 10;
-        border-radius: 0 0.5em 0.5em 0;
+        z-index: 5;
+        width: max(5vw, 5vh);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        div {
+            border-radius: 0 0.5em 0.5em 0;
+        }
     }
 
     .right {
         position: fixed;
-        top: 50%;
+        top: 0;
+        bottom: 0;
         right: var(--flyout-width);
-        // right: 0;
-        transform: translateY(-50%);
-        z-index: 10;
-        border-radius: 0.5em 0 0 0.5em;
+        z-index: 5;
+        width: max(5vw, 5vh);
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
         :global(.flanimate) & {
             transition: right 0.2s ease;
         }
+
+        div {
+            border-radius: 0.5em 0 0 0.5em;
+        }
     }
-    
+
     .hidden {
         opacity: 0;
     }
